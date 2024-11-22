21542859d1f780ef16e1540008575f5f1a152729
mspreeuwenberg <76214805+mspreeuwenberg@users.noreply.github.com>
2024-11-22 11:11:10 +0100

    Fix multiple fixes for default export (#2)
    * fix: use REGISTRATIENR instead of UID and remove custom code
    
    * fix: updated mapping to match fields of default export
    
    * feat: added option to exclude persons without contract
    
    * fix: fixed manager reference

--- persons.ps1
+++ persons.ps1
@@ -10,35 +10,25 @@ $persons = Import-CSV -Path "$csvDirectory\Tools4ever_Persons.csv" -Delimiter $c
 
 $csvContracts = Import-CSV -Path "$csvDirectory\Tools4ever_Contracts.csv"  -Delimiter $csvDelimiter
 $contracts = $csvContracts | Select-Object -Property *,@{Name = "functionExternalId"; Expression = { $_.WERKGEVERNR + "_" + $_.FUNCTIECODE } }
-$contracts = $contracts | Sort-Object UID
+$contracts = $contracts | Sort-Object REGISTRATIENR
 
 $csvFunctions = Import-CSV -Path "$csvDirectory\Tools4ever_Functions.csv"  -Delimiter $csvDelimiter 
 $functionList = $csvFunctions | Select-Object -Property *,@{Name = "externalId"; Expression = { $_.WERKGEVERNR + "_" + $_.FUNCTIECODE } } | Group-Object externalId -AsHashTable -AsString
 
 $csvOrganizations = Import-CSV -Path "$csvDirectory\Tools4ever_Organizations.csv"  -Delimiter $csvDelimiter | Group-Object WERKGEVERNR -AsHashTable
 
-#GJT(Kersten) -  Laag 2 en lager in motion OU ophalen tbv
-$csvBusinessUnits = Import-CSV -Path "$csvDirectory\Tools4ever_OrganizationalUnits.csv"  -Delimiter $csvDelimiter
-$businessUnitList = $csvBusinessUnits | Select-object -Property "HIERARCHIE_CD", "ORGOMS" | Where-Object { $_.HIERARCHIE_CD.length -le 11} | Sort-Object HIERARCHIE_CD -Unique | Group-Object HIERARCHIE_CD -AsHashTable -AsString
-
-$csvManagers = Import-CSV -Path "$csvDirectory\Tools4ever_Manager1.csv" -Delimiter $csvDelimiter
-#@GJT(Kersten) tijdelijk door ontbreken MAN_WERKGEVERNR deze onttrokken met $_.UID_MAN.Substring(0,5)
-$managerList = $csvManagers | Select-Object -Property *,@{Name = "employeeExternalId"; Expression = { $_.MED_WERKGEVERNR + "_" + $_.MED_REGISTRATIENR } }, @{Name = "managerExternalId"; Expression = { $_.UID_MAN.Substring(0,5) + "_" + $_.MAN_REGISTRATIENR } } | Group-Object employeeExternalId -AsHashTable -AsString
-#$managerList = $csvManagers | Group-Object employeeExternalId -AsHashTable -AsString
+$csvManagers = Import-CSV -Path "$csvDirectory\Tools4ever_Managers1.csv" -Delimiter $csvDelimiter
+$managerList = $csvManagers | Select-Object -Property *,@{Name = "employeeExternalId"; Expression = { $_.WERKGEVERNR + "_" + $_.MED_REGISTRATIENR } }, @{Name = "managerExternalId"; Expression = { $_.WERKGEVERNR + "_" + $_.MAN_REGISTRATIENR } } | Group-Object employeeExternalId -AsHashTable -AsString
 
 # add contracts, externalId and displayName properties to persons
 $persons | Add-Member -MemberType NoteProperty -Name "Contracts" -Value $null -Force
 $persons | Add-Member -MemberType NoteProperty -Name "ExternalId" -Value $null -Force
 $persons | Add-Member -MemberType NoteProperty -Name "DisplayName" -Value $null -Force
 
-# add function and organization description to contracts (And Business Unit)
+# add function and organization description to contracts
 $contracts | Add-Member -MemberType NoteProperty -Name "FunctionName" -Value $null -Force
 $contracts | Add-Member -MemberType NoteProperty -Name "OrganizationName" -Value $null -Force
 $contracts | Add-Member -MemberType NoteProperty -Name "ManagerExternalId" -Value $null -Force
-
-#GJT - add businessunit fields to contracts
-$contracts | Add-Member -MemberType NoteProperty -Name "BusinessUnitName" -Value $null -Force
-$contracts | Add-Member -MemberType NoteProperty -Name "BusinessUnitExternalId" -Value $null -Force
 
 # Enrich contracts with function and organization data
 $contracts | ForEach-Object {
@@ -57,50 +47,37 @@ $contracts | ForEach-Object {
 
     # Manager
 
-    $personManager = $managerList[$_.UID]
+    $personManager = $managerList[$_.REGISTRATIENR]
        
     if ($personManager.ManagerExternalId -is [system.array] ) {
-        # RJ - Todo? - Wellicht sort toevoegen?
         $_.ManagerExternalId = $personManager[0].ManagerExternalId
         $string = [string]$personManager.ManagerExternalId
-        if ($currentUID -ne $_.UID) {
-            # RJ - Todo? - Deze check buiten de loop plaatsen, wordt nu voor elk contract een foutmelding gegeven
-            Write-Verbose -Verbose "Multiple managers found for person $($_.UID): ($string). Keeping manager $($_.ManagerExternalId)"
+        if ($currentUID -ne $_.REGISTRATIENR) {
+            Write-Verbose -Verbose "Multiple managers found for person $($_.REGISTRATIENR): ($string). Keeping manager $($_.ManagerExternalId)"
         }
     } else {
         $_.ManagerExternalId = $personManager.ManagerExternalId
     }
-    $currentUID = $_.UID
-    
-    #GJT - BusinessUnit (Laag 2 van OU's in Motion)
-    if ($_.HIERARCHIE_CD.length -ge 11) {
-        $personBusinessUnit = $businessUnitList[$_.HIERARCHIE_CD.substring(0,11)]
-        if ($personBusinessUnit.count -eq 1) {
-            $_.BusinessUnitName = $personBusinessUnit.ORGOMS
-            $_.BusinessUnitExternalId  = $_.HIERARCHIE_CD.substring(0,11)
-        }
-    } else {
-        #GJT - If niet gevonden in Laag2 (directie bv) dan laag 1
-        $personBusinessUnit = $businessUnitList[$_.HIERARCHIE_CD.substring(0,6)]
-        if ($personBusinessUnit.count -eq 1) {
-            $_.BusinessUnitName = $personBusinessUnit.ORGOMS
-            $_.BusinessUnitExternalId  = $_.HIERARCHIE_CD.substring(0,6)
-        }
-    }
+    $currentUID = $_.REGISTRATIENR
 }
 
-# group contracts on UID
-$contracts = $contracts | Group-Object -Property UID -AsHashTable
-#write-verbose -verbose ($microsoftLicencesList | ConvertTo-Json)
+# group contracts on REGISTRATIENR
+$contracts = $contracts | Group-Object -Property REGISTRATIENR -AsHashTable
+
 # Add the enriched contracts to the person records
 $persons | ForEach-Object {
-    $_.ExternalId = $_.UID
+    $_.ExternalId = $_.REGISTRATIENR
     $_.DisplayName = $_.ROEPNM  + " " + $_.NAAMGEBRUIKVOORVOEGSEL  + " " + $_.NAAMGEBRUIKPERSOONNM  +" (" + $_.WERKGEVERNR + "_" + $_.REGISTRATIENR + ")"
     	
-    $personContracts = $contracts[$_.UID]
+    $personContracts = $contracts[$_.REGISTRATIENR]
     if ($null -ne $personContracts) {
         $_.Contracts = $personContracts
     }
+}
+
+if($config.ExcludePersonsWithoutContract)
+{
+    $persons = $persons | Where-Object -Property Contracts -ne $null
 }
 
 # Make sure persons are unique
